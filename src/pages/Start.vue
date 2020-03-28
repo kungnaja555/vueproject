9bb955e061f86ce11e4dde304d976c82f95b907d
kungnaja555 <pakasukkung@gmail.com>
2020-03-28 14:54:30 +0700

    28/3/2020 update

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,14 +1,64 @@
 <template>
-  <div @click="createSquare" @mousemove="mousemove">
-    <img class src="../assets/image/img.jpg" width="70%" id="img" />
-    <div :style="element"></div>
+  <div>
+    <v-card class="mx-auto" width="70%">
+      <v-stepper v-model="e1">
+        <v-stepper-header>
+          <v-stepper-step :complete="e1 > 1" step="1">Name of step 1</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step :complete="e1 > 2" step="2">Name of step 2</v-stepper-step>
+
+          <v-divider></v-divider>
+
+          <v-stepper-step step="3">Name of step 3</v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <v-card class="mb-12" color="grey lighten-1" height="470px"></v-card>
+
+            <v-btn color="primary" @click="setState(1)">เริ่ม</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <img
+              :src="'http://localhost:8000/image/'+img"
+              width="100%"
+              id="img"
+              @click="createSquare"
+              @mousemove="mouseMove"
+            />
+            <div :style="element"></div>
+
+            <v-btn color="primary" @click="setState(2)">กำหนดจุด</v-btn>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <img
+              :src="'http://localhost:8000/image/'+img"
+              width="100%"
+              id="img"
+              @click="createSquare"
+              @mousemove="mouseMove"
+            />
+            <div :style="element"></div>
+
+            <v-btn color="primary" v-if="state==2 || state==4" @click="setState(3)">เล่น</v-btn>
+            <v-btn color="primary" v-if="state==3" @click="setState(4)">หยุด</v-btn>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-card>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data() {
     return {
+      e1: 1,
       mouse: {
         x: "",
         y: "",
@@ -29,34 +79,27 @@ export default {
         y: 0,
         w: 0,
         h: 0
-      },
-      img: ""
+      }
     };
   },
-  created() {},
-  computed: {},
   methods: {
-    // logv(event) {
-    //   this.x = event.offsetX;
-    //   this.y = event.offsetY;
-    // },
-    createSquare(event) {
+    createSquare(e) {
       if (this.status) {
         this.status = false;
-        this.setposition();
+        this.setPosition();
       } else {
-        this.resetelement();
+        this.resetElement();
         this.status = true;
-        this.mouse.startX = event.offsetX;
-        this.mouse.startY = event.offsetY;
+        this.mouse.startX = e.offsetX;
+        this.mouse.startY = e.offsetY;
         this.element.border = "1px solid #ff0000";
         this.element.position = "absolute";
       }
     },
-    mousemove(event) {
+    mouseMove(e) {
       if (this.status) {
-        this.mouse.x = event.offsetX;
-        this.mouse.y = event.offsetY;
+        this.mouse.x = e.offsetX;
+        this.mouse.y = e.offsetY;
         this.element.width = Math.abs(this.mouse.x - this.mouse.startX) + "px";
         this.element.height = Math.abs(this.mouse.y - this.mouse.startY) + "px";
         this.element.left =
@@ -69,28 +112,52 @@ export default {
             : this.mouse.startY + "px";
       }
     },
-    resetelement() {
+    resetElement() {
       this.element.width = 0;
       this.element.height = 0;
       this.element.top = 0;
       this.element.left = 0;
       this.element.border = "";
     },
-    setposition() {
-      var img = document.getElementById("img");    
-      console.log("x " + parseFloat(this.element.width) * (img.naturalWidth / img.width));
-      console.log("y " + parseFloat(this.element.height) * (img.naturalWidth / img.width));
-      console.log("w " + parseFloat(this.element.left) * (img.naturalWidth / img.width));
-      console.log("h " + parseFloat(this.element.top) * (img.naturalWidth / img.width));
+    setPosition() {
+      var img = document.getElementById("img");
+      this.position.x =
+        parseFloat(this.element.width) * (img.naturalWidth / img.width);
+      this.position.y =
+        parseFloat(this.element.height) * (img.naturalWidth / img.width);
+      this.position.w =
+        parseFloat(this.element.left) * (img.naturalWidth / img.width);
+      this.position.h =
+        parseFloat(this.element.top) * (img.naturalWidth / img.width);
+    },
+    setState(state) {
+      var payout = {
+        state: state,
+        position: this.position
+      };
+      this.$store.dispatch("redis/setstate", payout);
+      if (state == 1) {
+        this.e1 = 2
+        this.$store.dispatch("redis/getimage");
+      }
+      if (state == 2) {
+        this.e1 = 3
+        this.$store.dispatch("redis/getimage");
+      }
     }
+  },
+  created() {
+    this.$store.dispatch("redis/getstate");
+  },
+  computed: {
+    ...mapState("redis", {
+      state: "state",
+      img: "img"
+    })
   }
 };
 </script>
 
 
 <style>
-.rectangle {
-  border: 1px solid #ff0000;
-  position: absolute;
-}
 </style>
--- src/store/modules/redis.js
+++ src/store/modules/redis.js
@@ -4,24 +4,36 @@ import api from '../../config/api'
 const redis = {
     namespaced: true,
     state: {
+        state: null,
         img: null
     },
     mutations: {
-        setimg(state,data){
-            console.log(data);          
+        setstate(state, data) {
+            state.state = data
+        },
+        setimage(state, data) {
             state.img = data
         }
     },
     getters: {
     },
     actions: {
-        getstartimg({commit}){
-            axios.get(api.port+"/redis/getstartimg")
-                .then(
-                    (data) => {
-                        commit('setimg',data.data)
-                    }
-                )
+        getstate({ commit }) {
+            axios.get(api.port + 'redis/getstate')
+                .then((data) => commit('setstate', data.data))
+        },
+        setstate({ commit }, payout) {
+            axios.post(api.port + 'redis/setstate', {
+                state: payout.state,
+                x: payout.position.x,
+                y: payout.position.y,
+                w: payout.position.w,
+                h: payout.position.h,
+            }).then((data) => commit('setstate', data.data))
+        },
+        getimage({ commit }) {
+            axios.get(api.port + 'redis/getimage')
+                .then((data) => commit('setimage', data.data))
         }
     }
 }
